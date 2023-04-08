28e77bfbceeac0ba41b7b36c2644baa97e223a91
Kyle Conquergood <kyleconquergood@gmail.com>
2023-04-08 00:06:36 -0700

    Initial Commit

---
+++ rinse/rinseApp.swift
@@ -0,0 +1,17 @@
+//
+//  rinseApp.swift
+//  rinse
+//
+//  Created by kyle on 2023-04-08.
+//
+
+import SwiftUI
+
+@main
+struct rinseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
