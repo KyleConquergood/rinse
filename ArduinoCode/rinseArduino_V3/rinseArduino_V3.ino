0cad1a954dd55a0e4bf88d83b191aec06dbd54d6
KyleConquergood <72111953+KyleConquergood@users.noreply.github.com>
2023-04-10 01:40:24 -0700

    Add files via upload

--- ArduinoCode/rinseArduino_V3/rinseArduino_V3.ino
+++ ArduinoCode/rinseArduino_V3/rinseArduino_V3.ino
@@ -1,33 +1,42 @@
 #include <ArduinoBLE.h>
 
-// Define the central device, service, and characteristics
+// Declare BLE objects
 BLEDevice central;
 BLEService sensorService("180D");
 BLEByteCharacteristic sensorDataCharacteristic("2A37", BLERead | BLENotify);
 BLEUnsignedIntCharacteristic timeStampCharacteristic("2A38", BLERead | BLENotify);
 
-// Define the time synchronization service and characteristic
 BLEService timeSyncService("180F");
 BLEUnsignedIntCharacteristic currentTimeCharacteristic("2A39", BLERead | BLENotify);
+BLEUnsignedIntCharacteristic syncTimeCharacteristic("2A3A", BLEWrite); // New characteristic to receive the synced Unix time
 
-// Variables to keep track of time
+
+// Declare variables for timing and syncing
 unsigned long currentTime = 0;
 unsigned long previousTime = 0;
 unsigned long startTime = 0;
-const long interval = 3000; // Interval for sending random sensor values (3 seconds)
+const long interval = 3000;
+
+// Function for handling incoming synced Unix time
+void syncTimeReceived(BLEDevice central, BLECharacteristic characteristic) {
+  startTime = *((unsigned long *) characteristic.value()) - (millis() / 1000);
+
+  // Print the value of startTime to the serial monitor
+  Serial.print("Received synced time: ");
+  Serial.println(startTime);
+}
 
 void setup() {
-  // Initialize serial communication
   Serial.begin(9600);
   while (!Serial);
 
-  // Initialize BLE and check for errors
+  // Initialize BLE module
   if (!BLE.begin()) {
     Serial.println("Starting BLE failed!");
     while (1);
   }
 
-  // Set the device name and advertised service
+  // Set BLE device name and advertised service
   BLE.setLocalName("Sensor Tracker");
   BLE.setAdvertisedService(sensorService);
 
@@ -36,48 +45,52 @@ void setup() {
   sensorService.addCharacteristic(timeStampCharacteristic);
   BLE.addService(sensorService);
 
-  // Add the time synchronization characteristic to the time sync service
+  // Add characteristics to the time sync service and set the event handler for synced time
   timeSyncService.addCharacteristic(currentTimeCharacteristic);
+  timeSyncService.addCharacteristic(syncTimeCharacteristic); // Add the new characteristic to the service
   BLE.addService(timeSyncService);
 
-  // Initialize characteristics with default values
+  syncTimeCharacteristic.setEventHandler(BLEWritten, syncTimeReceived); // Set the event handler for receiving the synced Unix time
+  Serial.println("Sync time event handler set"); 
+
+  // Initialize characteristics to zero and advertise BLE device
   sensorDataCharacteristic.writeValue(0);
   timeStampCharacteristic.writeValue(0);
 
-  // Start advertising the BLE device
   BLE.advertise();
   Serial.println("Bluetooth device active, waiting for connections...");
 }
 
 void loop() {
-  // Attempt to connect to the central device
+  // Check for central device connection
   central = BLE.central();
 
-  // Check if the central device is connected
   if (central) {
+    // Print connected central device address
     Serial.print("Connected to central: ");
     Serial.println(central.address());
 
-    // Loop while the central device is connected
+    // Loop while central device is connected
     while (central.connected()) {
       currentTime = millis();
-      if (startTime == 0) {
-        startTime = currentTime;
-      }
       
-      // Check if the interval has elapsed
+      // Check if interval has elapsed for updating sensor data
       if (currentTime - previousTime >= interval) {
         previousTime = currentTime;
-        
-        // Calculate the Unix timestamp based on the synced time
         unsigned long unixTime = startTime + (currentTime / 1000);
-        timeStampCharacteristic.writeValue(unixTime); // Send the Unix timestamp
+        timeStampCharacteristic.writeValue(unixTime);
 
-        // Generate a random sensor value between 0 and 255
         byte randomSensorValue = random(0, 256);
-        sensorDataCharacteristic.writeValue(randomSensorValue); // Send the random sensor value
+        sensorDataCharacteristic.writeValue(randomSensorValue);
+        
+        // Print the data and timestamp to the serial monitor
+        Serial.print("Data: ");
+        Serial.print(randomSensorValue);
+        Serial.print(" | Timestamp: ");
+        Serial.println(unixTime);
       }
     }
+    // Reset start time and print disconnected central device address
     startTime = 0;
     Serial.print("Disconnected from central: ");
     Serial.println(central.address());
